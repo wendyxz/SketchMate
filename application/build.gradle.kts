50e4fb67169688e4777c2c323c6275aaf30b3d35
Yihui Lu <yhlu@yhlu.home>
2023-03-09 22:54:07 -0500

    plugin/dependency for json serialization

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     kotlin("jvm") version "1.6.20"
     id("org.openjfx.javafxplugin") version "0.0.13"
     id("org.beryx.jlink") version "2.25.0"
+    id 'org.jetbrains.kotlin.plugin.serialization' version '1.6.10'
+
 }
 
 group = "net.codebot"
@@ -22,6 +24,7 @@ dependencies {
     implementation(project(":shared"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     testImplementation(kotlin("test"))
+    implementation "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2"
 }
 
 tasks.test {

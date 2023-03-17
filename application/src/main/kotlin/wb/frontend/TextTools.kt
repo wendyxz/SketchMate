f6d093b62c6204a64d51ddeba84396ca55c0359a
Edward-J-Xu <1614257233@qq.com>
2023-03-17 15:32:28 -0400

    Text styles

--- application/src/main/kotlin/wb/frontend/TextTools.kt
+++ application/src/main/kotlin/wb/frontend/TextTools.kt
@@ -2,10 +2,15 @@ package wb.frontend
 
 import javafx.geometry.Pos
 import javafx.scene.Scene
+import javafx.scene.control.ColorPicker
+import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
 import javafx.scene.control.TextArea
+import javafx.scene.layout.HBox
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
 import javafx.scene.transform.Scale
 
 
@@ -28,10 +33,25 @@ class TextTools(resizableCanvas: Pane) {
         textBox.isWrapText = true
         textBox.prefRowCount = 5
         textBox.prefColumnCount = 10
+
+        // Create font, color, and size controls
+        var fontComboBox = ComboBox<String>()
+        fontComboBox.items.addAll(Font.getFamilies())
+        fontComboBox.selectionModel.select("System")
+        var colorPicker = ColorPicker(Color.BLACK)
+        var sizeComboBox = ComboBox<Double>()
+        sizeComboBox.items.addAll(8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0)
+        sizeComboBox.selectionModel.select(12.0)
+
+        // Create a horizontal box to hold the controls
+        var controlsBox = HBox()
+        controlsBox.spacing = 10.0
+        controlsBox.children.addAll(fontComboBox, colorPicker, sizeComboBox)
+
         var group = VBox()
         group.alignment = Pos.TOP_CENTER
         var drag = Label("drag")
-        group.children.addAll(drag, textBox)
+        group.children.addAll(drag, controlsBox, textBox)
 
         drag.isVisible = false
         textBox.focusedProperty().addListener { obs, oldVal, newVal ->
@@ -45,5 +65,16 @@ class TextTools(resizableCanvas: Pane) {
         group.transforms.add(Scale(1.0 / scale.x, 1.0 / scale.y))
         group.transforms.add(scale)
         canvas.children.add(group)
+
+        // Add event listeners to update the text area's font, color, and size
+        fontComboBox.setOnAction {
+            textBox.style = "-fx-font-family: ${fontComboBox.value};"
+        }
+        colorPicker.setOnAction {
+            textBox.style = "-fx-text-fill: ${colorPicker.value};"
+        }
+        sizeComboBox.setOnAction {
+            textBox.style = "-fx-font-size: ${sizeComboBox.value}px;"
+        }
     }
 }

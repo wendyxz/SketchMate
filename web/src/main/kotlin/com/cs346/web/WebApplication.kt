8d47db66f875618da999a173b3683854776dd47a
Yihui Lu <yhlu@yhlu.home>
2023-03-20 23:48:59 -0400

    spring boot with JdbcTemplate

---
+++ web/src/main/kotlin/com/cs346/web/WebApplication.kt
@@ -0,0 +1,55 @@
+package com.cs346.web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import org.springframework.jdbc.core.query
+
+@SpringBootApplication
+class WebApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebApplication>(*args)
+}
+
+@RestController
+class MessageController(val service: MessageService) {
+    @GetMapping("/")
+    fun index(): List<Message> = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+}
+
+
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update("insert into messages values ( ?, ? )",
+            id, message.text)
+    }
+}
+
+
+data class Message(val id: String?, val text: String)
+
